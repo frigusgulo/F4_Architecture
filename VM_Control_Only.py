885c26bdfccd1cfa327770790501c0993831c5b1
Hill-Boss <37159382+Hill-Boss@users.noreply.github.com>
2019-04-04 18:31:55 -0600

    Create VM_Control_Only.py

---
+++ VM_Control_Only.py
@@ -0,0 +1,51 @@
+
+def Main():
+    pass
+
+def goto(labelname):
+    return "@" + str(labelname) + "\n0;JMP\n"
+
+def if_goto(labelname):
+    # pop_D() = "@SP\nAM=M-1\nD=M\nM=0\n"
+    return "@SP\nAM=M-1\nD=M\nM=0\n" + "D=D+1\n@" + str(labelname) + "\nD;JEQ\n"
+
+def label(labelname):
+    return "(" + str(labelname) + ")\n"
+
+callnum = -1
+def callFunction(FunctionName, nArgs):
+    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    callnum += 1
+    str = "@" + str(FunctionName) + callnum + "\nD=A\n" + "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    str += saveFrame(LCL) + saveFrame(ARG) + saveFrame(THIS) + saveFrame(THAT)
+    str += "@SP\nD=M\n@5\nD=D-A\n@" + int(nArgs) + "\nD=D-A\n@ARG\nM=D\n"
+    str += "@SP\nD=M\n@LCL\nM=D\n"
+    str += goto(str(FunctionName))
+    str += "(" + str(FunctionName) + callnum + ")\n"
+
+    return str
+
+def saveFrame(name):
+    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    return "@" + str(name) + "\nD=A\n" + "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+
+def makeFunction(FunctionName, nVars):
+    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    str = "(" + str(FunctionName) + ")\n"
+    for i in range(nVars):
+        str += "@0\nD=A\n" + "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+
+    return str
+
+def return():
+    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    str = "@LCL\nD=M\n@endFrame\nM=D\n"
+    str += "@5\nD=A\n@endFrame\nD=M-D\n@returnAddress\nM=D\n"
+    str += "@SP\nA=M\nM=D\n@SP\nM=M+1\n" + "@ARG\nM=D\n"
+    str += "@SP\nM=D+1\n"
+    str += "@endFrame\nD=M\n@THAT\nDM=D-1\n" + "@THIS\nDM=D-1\n" + "@ARG\nDM=D-1\n" + "@LCL\nM=D-1\n"
+    str += goto(returnAddress)
+    return str
+
+if __name__ == '__main__':
+    main()

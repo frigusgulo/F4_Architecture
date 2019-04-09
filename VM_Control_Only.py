99c8cdc6cc163216ce06a61856c3cbb78abc42a5
Hill-Boss <37159382+Hill-Boss@users.noreply.github.com>
2019-04-09 13:36:12 -0600

    Maybe fixed returnAddress in callfunction and return

--- VM_Control_Only.py
+++ VM_Control_Only.py
@@ -1,22 +1,25 @@
 
 def Main():
     pass
+# VM Control
 
 def goto(labelname):
     return "@" + str(labelname) + "\n0;JMP\n"
 
 def if_goto(labelname):
-    # pop_D() = "@SP\nAM=M-1\nD=M\nM=0\n"
-    return "@SP\nAM=M-1\nD=M\nM=0\n" + "@" + str(labelname) + "\nD;JGT\n"
+    return pop_D() + "D=D+1\n@" + str(labelname) + "\nD;JGT\n"
     # my understanding is if-goto jumps if top of stack is -1 (true) i.e. pop_D() + D=D+1 + D;JEQ
 
 def label(labelname):
     return "(" + str(labelname) + ")\n"
 
+RETURN_ADDRESSES = []
+
 callnum = -1
 def callFunction(FunctionName, nArgs):
     callnum += 1
     RETURN_ADDRESS = str(FunctionName) + str(callnum)
+    RETURN_ADDRESSES.append(RETURN_ADDRESS)
     str = "@" + RETURN_ADDRESS + "\nD=A\n" + push_D()
     str += saveFrame(LCL) + saveFrame(ARG) + saveFrame(THIS) + saveFrame(THAT)
     str += "@SP\nD=M\n@" + (5 + int(nArgs)) + "\nD=D-A\n@ARG\nM=D\n"
@@ -25,26 +28,24 @@ def callFunction(FunctionName, nArgs):
     str += "(" + RETURN_ADDRESS + ")\n"
     return str
 
+# Helper function for callFunction
 def saveFrame(name):
-    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    return "@" + str(name) + "\nD=A\n" + "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+    return "@" + str(name) + "\nD=M\n" + push_D()
 
 def makeFunction(FunctionName, nVars):
-    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    str = label(FunctionName)
+    str = label(str(FunctionName))
     for i in range(nVars):
-        str += "D=0\n" + "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
-    # label(FunctionName) + nVars * (D=0 + push_D())
+        str += "D=0\n" + push_D()
     return str
 
-def return():
-    # TODO push_D() "@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+def return_control():
     str = "@LCL\nD=M\n@endFrame\nM=D\n"
     str += "@5\nD=A\n@endFrame\nD=M-D\n@returnAddress\nM=D\n"
-    str += "@SP\nA=M\nM=D\n@SP\nM=M+1\n" + "@ARG\nM=D\n"
+    str += pop_D() + "@ARG\nM=D\n"
     str += "@SP\nM=D+1\n"
-    str += "@endFrame\nD=M\n@THAT\nDM=D-1\n" + "@THIS\nDM=D-1\n" + "@ARG\nDM=D-1\n" + "@LCL\nM=D-1\n"
-    str += goto(returnAddress)
+    str += "@endFrame\nD=M\n" + "@THAT\nDM=D-1\n" + "@THIS\nDM=D-1\n" + "@ARG\nDM=D-1\n" + "@LCL\nM=D-1\n"
+    str += goto(RETURN_ADDRESSES[-1])
+    RETURN_ADDRESSES.pop()
     return str
 
 if __name__ == '__main__':

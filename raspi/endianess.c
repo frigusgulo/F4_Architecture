7c5e6f75d025a5d18f9ef7b3145318c2a9391b2a
Hill-Boss <37159382+Hill-Boss@users.noreply.github.com>
2019-04-18 11:21:48 -0600

    fixed endianess

--- raspi/endianess.c
+++ raspi/endianess.c
@@ -2,8 +2,7 @@
 
 int main(void){
 	unsigned char *ptr;
-	int x,y,j;
-	int k=0;
+	int x,i;
 	int bigEndian = 0;
 	ptr = (unsigned char *)&x;
 
@@ -11,11 +10,13 @@ int main(void){
 	scanf("%d", &x);
 	printf("You entered: %d\n",x);
 	//Store in big endian
-	for( int i = 32; i >=0; i--){
-		 y = *(ptr+(k++));
-		 j = x >> i;
-		if( (j & y)!= y ){bigEndian = 1;}
-	}
+	for (i = 0; i < 4; i++)
+    		printf("   %p: %02x\n", ptr + i, *(ptr + i));
+
+  	bigEndian = (*ptr == (unsigned char)(0xff & (x >> 24))) &&
+            (*(ptr + 1) == (unsigned char)(0xff & (x >> 16))) &&
+            (*(ptr + 2) == (unsigned char)(0xff & (x >> 8))) &&
+            (*(ptr + 3) == (unsigned char)(0xff & x));
 	
 	if(bigEndian){printf("Your Computer is bigEndian\n");}
 	else{printf("Your Computer is littleEndian\n");}
